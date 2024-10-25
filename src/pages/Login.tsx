8a09990c141351ded12f40d8ce4dd722843bce07
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-10-25 12:31:12 +0600

    jwt decode added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,10 @@
 import { Button } from "antd";
 import Password from "antd/es/input/Password";
 import { useForm } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/features/auth/authSlice";
 
 const Login = () => {
   const { register, handleSubmit } = useForm({
@@ -11,12 +14,19 @@ const Login = () => {
     },
   });
 
+  const dispatch = useDispatch();
+
   const [login, { data, isError, isLoading, isSuccess, error }] =
     useLoginMutation();
 
   const onSubmit = async (data: { userId: string; password: string }) => {
-    console.log(isLoading, isError, isSuccess, error);
-    const res = await login(data).unwrap();
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = jwtDecode(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
 
   return (
